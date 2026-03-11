1b98d339faa255cf14667e6ed9f08eaa9aa16ef6
manh <manh99294@donga.edu.vn>
2026-03-11 23:47:26 +0700

    fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,7 +16,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173','https://portfolio-git-main-ducmanh99294s-projects.vercel.app'],
   credentials: true
 }))
 
